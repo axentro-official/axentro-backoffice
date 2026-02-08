929b7b7e1e5c9a943dd3fad583a51b73e65027d6
AXENTRO <axentroofficial@gmail.com>
2026-02-09 01:46:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
+// Axentro Backoffice - Runtime Config (GitHub Pages)
+
 window.AXENTRO_CONFIG = {
   GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyuNcLgIgoSH3kNUjGo9wQ2P4w25ifQsSK5XdhPCU4A0QxtpwZ2NOdt-KrpscwcP-iYew/exec",
   GOOGLE_CLIENT_ID: "778615894643-sbacc4bv7nlb6a11a1v5gj86i94lvb5f.apps.googleusercontent.com"
-};
-
-window.AXENTRO_CONFIG = {
-  GAS_WEB_APP_URL: "",
-  GOOGLE_CLIENT_ID: "",
 };
