3d59b34a27ba5c89a9ed921986eebaaa6d5efe9a
AXENTRO <axentroofficial@gmail.com>
2026-02-08 11:46:04 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Axentro Backoffice - One place to configure integrations
+// ضع القيم مرة واحدة فقط ثم ارفع الملف على GitHub
+window.AXENTRO_CONFIG = {
+  // مثال: https://script.google.com/macros/s/XXXX/exec
+  GAS_WEB_APP_URL: "",
+  // مثال: 1234567890-abcdefg.apps.googleusercontent.com
+  GOOGLE_CLIENT_ID: ""
+};
